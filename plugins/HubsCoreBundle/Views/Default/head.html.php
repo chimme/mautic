bda1033edcaf012c0b6fd8b813a762c35a2350a3
Arul raj <arulrajsm2802@gmail.com>
2016-12-12 12:36:44 +0530

    Overriding mautic page title

--- plugins/HubsCoreBundle/Views/Default/head.html.php
+++ plugins/HubsCoreBundle/Views/Default/head.html.php
@@ -19,6 +19,29 @@
     <?php $view['assets']->outputHeadDeclarations(); ?>
 
     <script>
+        if (typeof Mautic !== 'undeined' && typeof mQuery !== 'undeined') {
+            Mautic.generatePageTitle = function (route) {
+                if (-1 !== route.indexOf('view')) {
+                    //loading view of module title
+                    var currentModule = route.split('/')[3];
+
+                    //check if we find spans
+                    var titleWithHTML = mQuery('.page-header h3').find('span.span-block');
+                    var currentModuleItem = '';
+
+                    if (1 < titleWithHTML.length) {
+                        currentModuleItem = titleWithHTML.eq(0).text() + ' - ' + titleWithHTML.eq(1).text();
+                    } else {
+                        currentModuleItem = mQuery('.page-header h3').text();
+                    }
+
+                    mQuery('title').html(currentModule[0].toUpperCase() + currentModule.slice(1) + ' | ' + currentModuleItem + ' | 55 hubs - by 55 weeks');
+                } else {
+                    //loading basic title
+                    mQuery('title').html(mQuery('.page-header h3').html() + ' | 55 hubs - by 55 weeks');
+                }
+            };
+        }
         (function(t,a,l,k,u,s,e){if(!t[u]){t[u]=function(){(t[u].q=t[u].q||[]).push(arguments)},t[u].l=1*new Date();s=a.createElement(l),e=a.getElementsByTagName(l)[0];s.async=1;s.src=k;e.parentNode.insertBefore(s,e)}})(window,document,'script','//www.talkus.io/plugin.beta.js','talkus');
         talkus('init', 'qhmAwqHXMQeEy8oyK', {
             id: '<?php echo $app->getUser()->getUsername();?>',
