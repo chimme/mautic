aab2ee4aef880cbc7b188d453f8a9d3f65bc40a6
Arul Raj <arulrajsm2802@gmail.com>
2017-02-03 14:47:26 +0530

    remove onPLuginInstall function from bundle class

---
+++ plugins/HubsSlugGeneratorBundle/Config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+
+return [
+    'name'        => '55 hubs slug generator',
+    'description' => 'Add and manage slug field in contact.',
+    'version'     => '1.0',
+    'author'      => '55weeks',
+];
